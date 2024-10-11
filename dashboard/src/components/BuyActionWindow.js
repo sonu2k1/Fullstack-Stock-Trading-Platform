4907e3236551fb89b5aa1250b0d334f00a825a67
Sonu Singh <160756068+sonu2k1@users.noreply.github.com>
2024-10-11 12:21:00 +0530

    Update BuyActionWindow.js

--- dashboard/src/components/BuyActionWindow.js
+++ dashboard/src/components/BuyActionWindow.js
@@ -12,7 +12,7 @@ const BuyActionWindow = ({ uid }) => {
   const [stockPrice, setStockPrice] = useState(0.0);
 
   const handleBuyClick = () => {
-    axios.post("http://localhost:3002/newOrder", {
+    axios.post("https://creative-toffee-7ef474.netlify.app/newOrder", {
       name: uid,
       qty: stockQuantity,
       price: stockPrice,
